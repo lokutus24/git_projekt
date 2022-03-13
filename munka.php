c37f5e57449b513e9b28deef1a17802d70ba616f
Raska Gabor <raskagabor22@gmail.com>
2022-03-13 21:20:59 +0100

    remove anything

--- munka.php
+++ munka.php
@@ -1,14 +1,6 @@
 <?php
 
-//matamatika műveletek
-
-
-$elements = [2,3,4];
-array_filter($elements, function($a){
-
-	return $a*$a;
-});
-
+//for elements
 
 
 for ($i=0; $i <count($elements) ; $i++) { 
