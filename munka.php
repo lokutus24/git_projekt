a9a5d46b0b04a60a3c407ca8bd4fa3bb5355905f
Raska Gabor <raskagabor22@gmail.com>
2022-03-13 21:32:06 +0100

    new

--- munka.php
+++ munka.php
@@ -1,39 +1,8 @@
 <?php
 
 //for elements
-
-<<<<<<< HEAD
-
-<<<<<<< HEAD
 $elements = [2,3,4];
 array_filter($elements, function($a){
-=======
-//matamatika műveletek
->>>>>>> kivonas
 
 	return $a*$a;
 });
-
-
-<<<<<<< HEAD
-///ok
-=======
-//összeadás
-
-$a = 10;
-
-$b = 20;
-
-
-function method($a, $b){
-	return $a + $b;
-}
-
-echo method($a, $b);
->>>>>>> kivonas
-=======
-for ($i=0; $i <count($elements) ; $i++) { 
-	
-	echo $elements[$i]."\n";
-}
->>>>>>> raska
