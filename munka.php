a8d51a0ebc8c53e658028533f8ba73edc544efe5
Raska Gabor <raskagabor22@gmail.com>
2022-03-13 21:00:53 +0100

    matek

---
+++ munka.php
@@ -0,0 +1,17 @@
+<?php
+
+
+
+	//matamatika műveletek
+
+
+
+//összeadás
+
+$a = 10;
+
+$b = 20;
+
+
+
+echo $a+$b;
--- pepita/templates/admin.php
+++
@@ -1,4 +0,0 @@
-<div class="wrap">
-	<h1 class="wp-heading-inline">Pepita Feed Creator</h1>
-	<?php settings_errors();?>
-</div>
